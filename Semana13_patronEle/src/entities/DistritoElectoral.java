80ee13fef0e4867af9ec51c9bb618a44b7908a38
jasmt0411-collab <jasmt0411@gmail.com>
2026-08-11 21:48:16 -0600

    Etapa 1 del proyecto de Programación 3/Examen (#2)
    * This is just a test to see if the files have been loaded correctly
    
    * Este es el codigo de la primera etapa

---
+++ Semana13_patronEle/src/entities/DistritoElectoral.java
@@ -0,0 +1,36 @@
+package entities;
+
+public class DistritoElectoral {
+
+    private String codigoElectoral;
+    private String provincia;
+    private String canton;
+    private String distrito;
+
+    public DistritoElectoral(String codigoElectoral,
+                             String provincia,
+                             String canton,
+                             String distrito) {
+
+        this.codigoElectoral = codigoElectoral;
+        this.provincia = provincia;
+        this.canton = canton;
+        this.distrito = distrito;
+    }
+
+    public String getCodigoElectoral() {
+        return codigoElectoral;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public String getCanton() {
+        return canton;
+    }
+
+    public String getDistrito() {
+        return distrito;
+    }
+}
